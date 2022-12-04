f314da55f6a2948cf18d54745be250cfb58b40b3
martin-ngigi <martinwainaina001@gmail.com>
2022-12-04 17:30:38 +0300

    Added Form... Calling forms from the controller success

--- resources/views/forms.blade.php
+++ resources/views/forms.blade.php
@@ -1,6 +1,8 @@
 <h1>User Login</h1>
 
-<!-- "get-users" is the url defined in the routes -->
+<!-- "get-users" is the url defined in the routes
+when btn is clicked, it calls the "/get-users" url which invokes method getMyData in the Users Controller...
+The getMyData method in the User controller returns a string. -->
 <form action="/get-users" method="POST">
     @csrf
     <input type="text" name="username" placeholder="Enter Username"/><br>
