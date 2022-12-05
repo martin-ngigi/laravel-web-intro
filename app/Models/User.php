08b6b9439404656bbaef3bc69b9c43d2fc364782
martin-ngigi <martinwainaina001@gmail.com>
2022-12-05 15:14:03 +0300

    Added a comment

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     use HasFactory;
+    /**
+     * Incase the table name dont match with model name...
+     * One can define the table name here
+     * eg the model name is User but the db table name is Employees
+     */
+    
+    //public $table = "employees";
+
 }
